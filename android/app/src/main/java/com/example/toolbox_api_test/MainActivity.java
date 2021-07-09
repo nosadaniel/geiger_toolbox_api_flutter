ca1f0463352b1819a4fdfcf64231724784af13bb
Luong NGUYEN <luongnv89@gmail.com>
2021-07-09 12:17:53 +0200

    first commit

---
+++ android/app/src/main/java/com/example/toolbox_api_test/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.toolbox_api_test;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+
+//Geiger packages
+import ch.fhnw.geiger.localstorage.StorageException;
+import eu.cybergeiger.communication.Declaration;
+import eu.cybergeiger.communication.DeclarationMismatchException;
+import ch.fhnw.geiger.localstorage.db.GenericController;
+import eu.cybergeiger.communication.LocalApi;
+import eu.cybergeiger.communication.LocalApiFactory;
+import ch.fhnw.geiger.localstorage.db.data.Node;
+import ch.fhnw.geiger.localstorage.db.data.NodeImpl;
+
+
+public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "com.geiger.dev/toolboxAPI";
+
+  private LocalApi localApi;
+  private GenericController genericController;
+
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+  super.configureFlutterEngine(flutterEngine);
+    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+        .setMethodCallHandler(
+          (call, result) -> {
+            // Note: this method is invoked on the main thread.
+            if (call.method.equals("getStorage")) {
+              String storageMsg = getStorage();
+              if (!storageMsg.isEmpty()) {
+                result.success(storageMsg);
+              } else {
+                result.error("UNAVAILABLE", "Storage not created.", null);
+              }
+            } else {
+              result.notImplemented();
+            }
+          }
+
+        );
+  }
+
+  private String getStorage(){
+    try{
+        System.out.println("LocalAPI variable: " + LocalApi.MASTER);
+        this.localApi = LocalApiFactory.getLocalApi("mi-cyberrange", LocalApi.MASTER, Declaration.DO_NOT_SHARE_DATA);
+        // this.genericController = (GenericController) localApi.getStorage();
+
+        // //create node
+        Node userUUIdNode = new NodeImpl("328161f6-89bd-49f6-user-5a375ff56ana", ":Users");
+        // //add node to database
+        // this.genericController.add(userUUIdNode);
+        // this.genericController.update(userUUIdNode);
+
+        return LocalApi.MASTER;
+    }
+    catch (StorageException | DeclarationMismatchException se){
+        se.printStackTrace();
+        return  "Sorry!";
+    }
+}
+
+  private int getBatteryLevel() {
+    int batteryLevel = -1;
+    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+      BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+    } else {
+      Intent intent = new ContextWrapper(getApplicationContext()).
+          registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+      batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+          intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+    }
+
+    return batteryLevel;
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  var _testing_number = '0.1';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter - Toolbox APIs',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: MyHomePage(title: 'Flutter - Toolbox APIs test ' + _testing_number),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = const MethodChannel('com.geiger.dev/toolboxAPI');
+
+  // Get battery level.
+  String _getStorageStatus = 'Unknown getStorage status.';
+
+  Future<void> _getStorage() async {
+    String status;
+    try {
+      print('Going to get the storage status...');
+      final String result = await platform.invokeMethod('getStorage');
+      status = 'Storage status: $result .';
+    } on PlatformException catch (e) {
+      status = "Failed to get storage status: '${e.message}'.";
+    }
+
+    setState(() {
+      _getStorageStatus = status;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              child: Text('Get Storage'),
+              onPressed: _getStorage,
+            ),
+            Text(_getStorageStatus),
+          ],
+        ),
+      ),
+    );
+  }
+}
